fa28d280902b4827dccf18d4fc54ea0ae9913f8a
abdubash04 <165327233+abdubash04@users.noreply.github.com>
2024-03-28 21:18:58 +0000

    i have created list of modules

---
+++ modules.js
@@ -0,0 +1,81 @@
+let modules = [
+    {
+        id: 1,
+        title: "Mathematics",
+        price: 50.5,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 2,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+
+    {
+        id: 1,
+        title: "",
+        price: 222,
+        availableInventory: 7,
+        Image: ""
+    },
+]
