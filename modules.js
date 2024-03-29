4ee0991c69e434c9c558daef5437acc842b5b7e6
abdubash04 <165327233+abdubash04@users.noreply.github.com>
2024-03-29 19:55:24 +0100

    Finished setting up Module list

--- modules.js
+++ modules.js
@@ -3,79 +3,91 @@ let modules = [
         id: 1,
         title: "Mathematics",
         price: 50.5,
-        availableInventory: 7,
-        Image: ""
+        location: "Jam'are",
+        icon: "bi bi-calculator",
+        spaces: 8
     },
 
     {
         id: 2,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        title: "Robotics",
+        price: 100,
+        location: "DIAC",
+        icon: "bi bi-robot",
+        spaces: 5
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 3,
+        title: "Literature",
+        price: 80,
+        location: "DIAC",
+        icon: "bi bi-book-half",
+        spaces: 5
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 4,
+        title: "cooking",
+        price: 20,
+        location: "DIAC",
+        icon: "bi bi-egg-fried",
+        spaces:10,
+
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 5,
+        title: "Driving",
+        price: 100,
+        location: "DIAC",
+        icon: "bi bi-car-front",
+        spaces: 5
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 6,
+        title: "Arts and Craft",
+        price: 40,
+        location: "DIAC",
+        Icon: "bi bi-brush",
+        spaces: 6
+
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 7,
+        title: "Trading",
+        price: 120,
+        location: "DIAC", 
+        Icon: "bi bi-graph-up",
+        spaces: 10
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 8,
+        title: "HTML",
+        price: 20,
+        location: "DIAC",
+        Icon: "bi bi-filetype-html",
+        spaces: 8
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 9,
+        title: "Communication Skills",
+        price: 50,
+        location: "Online",
+        Icon: "bi bi-chat-left-text",
+        spaces: 5
     },
 
     {
-        id: 1,
-        title: "",
-        price: 222,
-        availableInventory: 7,
-        Image: ""
+        id: 10,
+        title: "sports Drills",
+        price: 20,
+       location: "DIAC Park",
+        Icon: "bi bi-cone-striped",
+        spaces: 10
     },
 ]
